63c405b5cff87a0d04ca50c1b896010f40d2b94a
arjun8078 <arjunkanhangad143@gmail.com>
2026-04-27 18:58:40 +0530

    Production environment added

---
+++ frontend/src/environment/environment.production.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://13.62.229.223:5000/api'
+};
--- frontend/src/environment/environment.ts
+++ frontend/src/environment/environment.ts
@@ -3,3 +3,8 @@ export const environment = {
   apiUrl: 'http://localhost:5000/api'  // Your backend URL
 
 };
+
+// export const environment = {
+//   production: true,
+//   apiUrl: 'http://13.62.229.223:5000/api'
+// };
